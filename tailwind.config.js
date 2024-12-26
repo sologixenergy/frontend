1da3464a13c32ea6ae59c4f9d3f54ee58def2b5a
web-vikas <msg_vikas@yahoo.com>
2024-12-26 14:17:19 +0530

    Update Tailwind configuration to refine content paths and include NextUI theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,9 @@
 import { nextui } from "@nextui-org/react";
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    // "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,jsx}",
+    "./src/app/**/*.{js,jsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
